97ed2a5ff868b19ddd026a4970bcbbc08283c76d
AdamSkuse <a.t.skuse@gmail.com>
2017-03-06 16:34:04 +0000

    Adds Twitter button

--- script.js
+++ script.js
@@ -10,13 +10,25 @@ var getQuoteButton = document.getElementById("getQuoteButton");
 // 
 $(document).ready(function() {
 
-    $("#getQuoteButton").on("click", function(){
+    $("#getQuoteButton").on("click", function() {
         $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?", function(json) {
             console.log(JSON.stringify(json));
             console.log(json.quoteText);
             $("#quoteDisplay").html(json.quoteText);
-
+            $("#authorName").html(json.quoteAuthor);
+            createTwitterButton(json.quoteText, json.quoteAuthor);
     });
   });
+
+    function createTwitterButton(quoteText, quoteAuthor){
+        var link = document.createElement("a");
+        var twitterShareDiv = document.getElementById("twitterShareDiv");
+        link.setAttribute("class", "twitter-share-button");
+        link.setAttribute("href", "https://twitter.com/share");
+        link.setAttribute("data-text", quoteText + "  - " + quoteAuthor);
+        twitterShareDiv.removeChild(twitterShareDiv.childNodes[0]);
+        twitterShareDiv.appendChild(link);
+        twttr.widgets.load();
+    }
 });
 
